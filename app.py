00e27ba215120171302d90c022d1e4fef265cea5
ayushach007 <ayushach007@gmail.com>
2024-11-16 12:32:02 +0545

    modified app.py

--- app.py
+++ app.py
@@ -17,49 +17,66 @@ def main():
     
     try:
     
-        st.title("**Resume Builder and Reviewer**")
+        # title for the app
+        st.markdown("<h1 style='text-align: center;'>📄 Resume Improvement Tool 📄</h1>", unsafe_allow_html=True)
         
-        api_key = st.sidebar.text_input("Enter your API key", type='password')
-        
+        # select the LLM tool
         tool = st.sidebar.selectbox("Select the LLM tool", ['OpenAI', 'GROQ'])
         
+        # select the model
         if tool.lower() == 'openai':
             model = st.sidebar.selectbox("Select the model", ['gpt-4o', 'gpt-4.5-turbo', 'gpt-4-turbo', 'gpt-3.5-turbo'])
         else:
             model = st.sidebar.selectbox("Select the model", ['gemma2-9b-it', 'llama3-groq-70b-8192-tool-use-preview', 'llama-3.1-8b-instant', 'llama3-8b-8192', 'llava-v1.5-7b-4096-preview'])
         
-        _llm = initialize_llm(tool=tool, api_key=api_key, model=model)
+        # input for the API key
+        api_key = st.sidebar.text_input("Enter your API key", type='password')
         
-        resume = upload_resume()
-        
-        if resume:
-            url = st.text_input("Enter the URL of the job description", "https://www.example.com")
-            job_description = extract_job_description(_llm, url)
-        
-            parsed_resume_review = review_resume(_llm, resume, job_description)
+        # check if the API key is provided
+        if api_key:
             
-            if 'generate_new_resume' not in st.session_state:
-                st.session_state.generate_new_resume = False
+            # initialize the LLM model
+            _llm = initialize_llm(tool=tool, api_key=api_key, model=model)
+            
+            # upload the resume
+            resume = upload_resume()
+            
+            # check if the resume is uploaded
+            if resume:
+                # input for the job description URL
+                url = st.text_input("Enter the URL of the job description", "https://www.example.com")
                 
-            if st.button("Generate New Resume"):
-                st.session_state.generate_new_resume = True
+                # extract the job description
+                job_description = extract_job_description(_llm, url)
+
+                # review the resume
+                parsed_resume_review = review_resume(_llm, resume, job_description)
                 
-            if st.session_state.generate_new_resume:
-                # Generate PDF from improved_resume
-                improved_resume = build_resume(_llm, resume, job_description, parsed_resume_review)
                 
-                st.code(improved_resume)
-                
-                if 'generate_feedback' not in st.session_state:
-                    st.session_state.generate_feedback = False
-                
-                if st.button("Generate Feedback"):
-                    st.session_state.generate_feedback = True
+                if 'generate_new_resume' not in st.session_state:
+                    st.session_state.generate_new_resume = False
                     
-                if st.session_state.generate_feedback:
-                    old_and_new_resume_review_output = compare_resumes(_llm, resume, improved_resume, job_description)
-                    st.write('Old and New Resume Review \n')
-                    st.code(old_and_new_resume_review_output)
+                if st.button("Generate New Resume"):
+                    st.session_state.generate_new_resume = True
+                    
+                if st.session_state.generate_new_resume:
+                    improved_resume = build_resume(_llm, resume, job_description, parsed_resume_review)
+                    
+                    st.code(improved_resume)
+                    
+                    if 'generate_feedback' not in st.session_state:
+                        st.session_state.generate_feedback = False
+                    
+                    if st.button("Generate Feedback"):
+                        st.session_state.generate_feedback = True
+                        
+                    if st.session_state.generate_feedback:
+                        old_and_new_resume_review_output = compare_resumes(_llm, resume, improved_resume, job_description)
+                        st.write('Old and New Resume Review \n')
+                        st.code(old_and_new_resume_review_output)
+        else:
+            st.info(":warning: Please enter your API key to proceed.")
+            
     except Exception as e:
         st.error(f"An error occurred: {e}")
         
